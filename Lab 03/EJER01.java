1b9a028865fb85e5425800a5ac43ac4a4c710a5b
Kevin Romero <108586694+usuarioanonimoparticular@users.noreply.github.com>
2023-09-03 19:49:03 -0500

    Add files via upload

---
+++ Lab 03/EJER01.java
@@ -0,0 +1,83 @@
+public class Coodenada {
+    private double X;
+    private double Y;
+
+    public Coodenada ( double X, double Y){
+        this.X = x;
+        this.Y = y;
+    }
+    public Coodenada (x: int, y:int){}
+
+    public Coodenada ( c : Coodenada){
+        this.c : Coodenada = c1 : Coodenada;
+        this.c : Coodenada = c2 : Coodenada;
+    }
+
+    public double getX ()
+    { return x;}
+
+    public void setX(double x)
+    { this.X = x;}
+
+    public double getY ()
+    { return y;}
+
+    public void setY(double y)
+    { this.Y = y;}
+
+    double perimetro = (2*X) + (2*Y);
+    double s = perimetro; 
+    
+    public distance (c: Coodenada ) : double;
+
+    public static distance (c1: Coodenada, c2: Coodenada) : double; 
+    // calcula la distancia entre 2 coordenadas en un plano carteciano
+    
+    
+    @override
+    public string tostring ()
+    { toString s = string.format(this.X, this.Y)
+        return s;
+    }
+}
+
+public class Rectangulo {
+    double esq01 : Coodenada;
+    double esq02 : Coodenada;
+
+    public Rectangulo (c1: Coodenada, c2: Coodenada, ){
+        this.c1:Coodenada = esq01:Coodenada;
+        this.c2:Coodenada = esq02: Coodenada;
+    }
+
+    public double getesq01():Coodenada {
+        return esq01;
+    }
+    
+    public void setesq01(c1 :Coodenada){
+        this.c1:Coodenada = esq01:Coodenada;
+    }
+
+
+    public double getesq02():Coodenada {
+        return esq02;
+    }
+    
+    public void setesq02(c2 :Coodenada){
+        this.c2:Coodenada = esq02:Coodenada;
+    }
+
+}
+
+public class Principal {
+    public static main (string[] args);
+    double long = math.abs(c1 - esq01);
+    double ancho = math.abs(c2 - esq02);
+    double area =long * ancho;
+    System.out.println("El área del rectángulo es: " + area);
+    
+}
+
+public class Manejador {
+    
+}
