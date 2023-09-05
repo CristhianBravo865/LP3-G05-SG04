6d9e64dbec82eef4272f393cadd3738a1f795ece
CristhianBravo865 <142510291+CristhianBravo865@users.noreply.github.com>
2023-09-04 19:45:38 -0500

    Add files via upload

--- Lab 03/Ejercicio 1/Principal.java
+++ Lab 03/Ejercicio 1/Principal.java
@@ -1,15 +1,13 @@
 public class Principal{
     public static void main(String[] args) {
-        Rectangulo a,b;
-        Coordenada c1,c2,c3,c4;
-        c1=new Coordenada(0, 0);
-        c2=new Coordenada(1, 2);
-        c3=new Coordenada(2, 0);
-        c4=new Coordenada(4, 2);
-        a=new Rectangulo(c1,c2);
-        b=new Rectangulo(c3,c4);
-        Coordenada.distancia(c1, c2);
-        System.out.println(Coordenada.distancia(c3, c4));
+        Rectangulo a,b,c;
+        Coordenada c5;
+        c5=new Coordenada(4,2);
+        a=new Rectangulo(c5);
+        b=new Rectangulo(c5);
+        c=new Rectangulo(c5);
         System.out.println(a.toString());
+        System.out.println(b.toString());
+        System.out.println(c.toString());
     }
 }
--- Lab 03/Ejercicio 1/Rectangulo.java
+++ Lab 03/Ejercicio 1/Rectangulo.java
@@ -1,14 +1,30 @@
 public class Rectangulo {
     private Coordenada x,y;
-    
+    private String color;
+    private static String ultimocolor="Rojo";
+    private void CreacionColor(){
+        if (ultimocolor=="Verde"){
+            ultimocolor="Amarillo";
+        }
+        else if (ultimocolor=="Amarillo"){
+            ultimocolor="Rojo";
+        }
+        else if (ultimocolor=="Rojo"){
+            ultimocolor="Verde";
+        }
+    }
     public Rectangulo(Coordenada c1, Coordenada c2){
         this.x=c1;
         this.y=c2;
+        CreacionColor();
+        this.color=ultimocolor;
     }
     public Rectangulo(Coordenada ba){
-        this.x.setX(0);
-        this.x.setY(0);
+        Coordenada n = new Coordenada(0,0);
+        this.x=n;
         this.y=ba;
+        CreacionColor();
+        this.color=ultimocolor;
     }
     public Coordenada getX() {
         return x;
@@ -24,6 +40,6 @@ public class Rectangulo {
     }
     @Override
     public String toString(){
-        return "Las coordenadas de las esquinas son "+x+" , "+y;
+        return "Las coordenadas de las esquinas son "+this.x+" , "+this.y+" , su color es "+this.color;
     }
 }
